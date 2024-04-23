982964ffb05f917d06912a37ae1e6bb934bb784f
ElfCutieCelly <elfcelly0624@gmail.com>
2024-04-23 15:27:01 -0700

    womp womp

--- lab4/js/lab3.js
+++ lab4/js/lab3.js
@@ -1,1 +1,15 @@
-// 2
+// Hangman
+//A Guessing Game
+//Celeste Hernandez Emily Belezzuoli
+//April 23,2024
+
+//Set up a word for someone to guess
+    //Set up podium( a 7)
+    //Guess letters
+    //If true add letters to blanks,
+        //For each letter wrong, draw body part of man
+            //For the first incorrect guess: head
+            //For the second incorrect guess: body
+            //For the third + fourth incorrect guess: arms
+            //For the fifth + six incorrect guess: legs
+    //If you guess incorrectly six times before guessing the selected word: YOU LOSE!
