0d68e8fc9a5c1308d2af4264a6854ab77d530f4d
ElfCutieCelly <elfcelly0624@gmail.com>
2024-04-23 14:32:30 -0700

    We must commit

---
+++ lab4/js/lan2.js
@@ -0,0 +1,13 @@
+//Stipllisim 
+//The basic beggining skills
+//Celeste Hernandez & Emily Belezzuoli
+//April 23,2024
+//Pick paper
+    //Pick pens/width
+        //Obtain/Sketch outline
+            //Focus on tones and lighting
+            ///Dot pattern/condensed or uncondensed
+        //Frequency of dots
+        //Look at work from certain angle
+    //If needed, add more depth into work
+//Admire amd then Finish 
