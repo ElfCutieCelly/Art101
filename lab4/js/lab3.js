d31f41d395c70528ea97fa4a216aa5fde1889eb6
ElfCutieCelly <elfcelly0624@gmail.com>
2024-04-23 15:32:16 -0700

    yes

--- lab4/js/lab3.js
+++ lab4/js/lab3.js
@@ -1,3 +1,21 @@
+
+//Everyday Task
+//Stipllisim 
+//The basic beggining skills
+//Celeste Hernandez & Emily Belezzuoli
+//April 23,2024
+//Pick paper
+    //Pick pens/width
+        //Obtain/Sketch outline
+            //Focus on tones and lighting
+            ///Dot pattern/condensed or uncondensed
+        //Frequency of dots
+        //Look at work from certain angle
+    //If needed, add more depth into work
+//Admire amd then Finish 
+
+
+//Computer Games
 // Hangman
 //A Guessing Game
 //Celeste Hernandez Emily Belezzuoli
