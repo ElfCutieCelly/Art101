4d9b7d9a346fa551ea37611180a51b37a72c032a
ElfCutieCelly <elfcelly0624@gmail.com>
2024-04-24 23:37:39 -0700

    Update lab3.js

--- lab4/js/lab3.js
+++ lab4/js/lab3.js
@@ -1,33 +1,34 @@
 
 //Everyday Task
-//Stipllisim 
-//The basic beggining skills
+//Stippling
+//The basic beginning skills
 //Celeste Hernandez & Emily Belezzuoli
 //April 23,2024
 //Pick paper
-    //Pick pens/width
-        //Obtain/Sketch outline
-            //Focus on tones and lighting
-            ///Dot pattern/condensed or uncondensed
-        //Frequency of dots
-        //Look at work from certain angle
-    //If needed, add more depth into work
-//Admire amd then Finish 
+   //Pick pens/width
+       //Obtain/Sketch outline
+           //Focus on tones and lighting
+           ///Dot pattern/condensed or uncondensed
+       //Frequency of dots
+       //Look at work from certain angle
+   //If needed, add more depth into work
+//Admire amd then Finish
 
 
-//Computer Games
+
 // Hangman
 //A Guessing Game
 //Celeste Hernandez Emily Belezzuoli
 //April 23,2024
 
+
 //Set up a word for someone to guess
-    //Set up podium( a 7)
-    //Guess letters
-    //If true add letters to blanks,
-        //For each letter wrong, draw body part of man
-            //For the first incorrect guess: head
-            //For the second incorrect guess: body
-            //For the third + fourth incorrect guess: arms
-            //For the fifth + six incorrect guess: legs
-    //If you guess incorrectly six times before guessing the selected word: YOU LOSE!
+   //Set up podium( a 7)
+   //Guess letters
+   //If true add letters to blanks,
+       //For each letter wrong, draw body part of man
+           //For the first incorrect guess: head
+           //For the second incorrect guess: body
+           //For the third + fourth incorrect guess: arms
+           //For the fifth + six incorrect guess: legs
+   //If you guess incorrectly six times before guessing the selected word: YOU LOSE!
