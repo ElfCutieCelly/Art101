be50dcabdf472fdacdacb692aafcad0776af0702
ElfCutieCelly <elfcelly0624@gmail.com>
2024-04-25 15:24:00 -0700

    Create lab5.js

---
+++ lab5/js/lab5.js
@@ -0,0 +1,21 @@
+//
+ //Author: Celeste Hernandez <ceanhern@ucsc.edu
+ //Created: April 25, 2024
+ //Licence: n/a
+//
+
+// Define Variables
+make= "BMW";
+modle="Cancer";
+color="Rose Gold";
+year=1995;
+
+//calculate
+age = 2024 - year;
+
+//output
+document.writeln("Make: "+ make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: "+ year + "<br>");
+document.writeln("Age: " + age + "years<br>");
