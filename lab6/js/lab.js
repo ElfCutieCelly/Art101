73b76adcd348c591ab0574ad71733e905e3008f0
ElfCutieCelly <elfcelly0624@gmail.com>
2024-04-27 13:02:14 -0700

    Update lab.js

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,25 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 6 Jacvascript Programing
+// Author: Celeste Hernandez
+// Date: 27 April
 
-// Constants
+//Define Variables
+myTransport = ["Walking", "Bike", "Walking", "Running", "Car"];
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+//create an object for my main ride
+myMainRide = {
+  make: "Kia",
+  model: "Soul",
+  color: "Rose Gold",
+  year: "1995",
+  //we can define a funtion withing our object (called a method)
+  //that uses the value of yea above (usuing this.year)
+  age: function() {
+    return 2022- this.year;
+  }
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//output
+document.writeln("Kinds of Transportations I use: ",myTransport,"</br>");
+//this little trick allows use to write ab objecrt to the document
+document.writeln(" My main Ride: <pre>,"
+    .stringify(myMainRide, null, '\t'),"</pre>")
