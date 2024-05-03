3d73a532e5fe7da36e9285f5ce94b908333c839e
ElfCutieCelly <elfcelly0624@gmail.com>
2024-05-02 22:32:11 -0700

    Update lab7.js

--- lab7/js/lab7.js
+++ lab7/js/lab7.js
@@ -7,14 +7,14 @@
 //sortuserName - a function that takes user input and sorts the letters
 function sortUserName(){
     //retrieve.user's name
-    let userName = window.promt("What's your name?");
+    let userName = window.prompt("What's your name?");
     console.log(userName);
     //sort and return users name
-    let sortedName = userName.split('').sort().join('');
-    console.log(sortedName);
+    let sortName = userName.split('').sort().join('');
+    console.log(sortName);
     //return sorted name
     return sortedName;
 };
 
 //output
-document.writeln("No, your name is",sortedName());
+document.writeln("No, your name is",sortName());
