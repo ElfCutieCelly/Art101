baba9fc0200124d4052f890e702a73e89ecd5561
ElfCutieCelly <elfcelly0624@gmail.com>
2024-05-03 00:59:15 -0700

    I hate it here

--- lab7/js/lab7.js
+++ lab7/js/lab7.js
@@ -5,16 +5,21 @@
 //
 
 //sortuserName - a function that takes user input and sorts the letters
-function sortUserName(){
+function sortUserName() {
     //retrieve.user's name
-    let userName = window.prompt("What's your name?");
-    console.log(userName);
-    //sort and return users name
-    let sortName = userName.split('').sort().join('');
-    console.log(sortName);
-    //return sorted name
-    return sortedName;
-};
-
+    let userName = window.prompt("Hi. Please tell me your name so I can fix it.");
+    console.log("userName =", userName);
+    //split string to array
+    let nameArray = userName.split('');
+    console.log("nameArray=", nameArray),
+    //sort the array
+    let nameArraySort = nameArray.sort();
+    console.log("nameArraySort=", nameArraySort);
+    //join array back to a string
+    let nameSorted = nameArraySort.join("");
+    console.log("nameSorted=", nameSorted);
+    return nameSorted;
+}
 //output
-document.writeln("No, your name is",sortName());
+document.writeln("Oh hey, I've fixed your name: ",
+sortUserName(), "<br>");
