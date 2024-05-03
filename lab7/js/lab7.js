0a68216b585d67c9c4cdeb7465f9b60b2a3df7a7
ElfCutieCelly <elfcelly0624@gmail.com>
2024-05-02 20:46:07 -0700

    Update lab7.js

--- lab7/js/lab7.js
+++ lab7/js/lab7.js
@@ -1,11 +1,12 @@
 // index.js - Lab 7 Functions
 // Author: Celeste Hernandez
 // Date: 2 May
-    //
+//
 //
 
 //sortuserName - a function that takes user input and sorts the letters
 function sortUserName(){
+    //retrieve.user's name
     let userName = window.promt("What's your name?");
     console.log(userName);
     //sort and return users name
@@ -16,4 +17,4 @@ function sortUserName(){
 };
 
 //output
-document.writeln ("Oh hey, I've fixed your name:",sortedName());
+document.writeln("No, your name is",sortedName());
