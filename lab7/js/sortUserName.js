78faf5d163633c9b340e257b0f51c602c83af48c
ElfCutieCelly <elfcelly0624@gmail.com>
2024-05-03 01:02:21 -0700

    i wanna kill myself

---
+++ lab7/js/sortUserName.js
@@ -0,0 +1,21 @@
+// index.js - Lab 7 Functions
+// Author: Celeste Hernandez
+// Date: 2 May
+//
+//
+//sortuserName - a function that takes user input and sorts the letters
+function sortUserName() {
+    //retrieve.user's name
+    let userName = window.prompt("Hi. Please tell me your name so I can fix it.");
+    console.log("userName =", userName);
+    //split string to array
+    let nameArray = userName.split('');
+    console.log("nameArray=", nameArray),
+        //sort the array
+        let; nameArraySort = nameArray.sort();
+    console.log("nameArraySort=", nameArraySort);
+    //join array back to a string
+    let nameSorted = nameArraySort.join("");
+    console.log("nameSorted=", nameSorted);
+    return nameSorted;
+}
