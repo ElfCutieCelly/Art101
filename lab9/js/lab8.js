d24533f2c76360ebe5c1b05edc2e1b09794174cc
ElfCutieCelly <elfcelly0624@gmail.com>
2024-05-12 10:29:38 -0700

    Update lab8.js

---
+++ lab9/js/lab8.js
@@ -0,0 +1,16 @@
+// 
+
+//locationbar.js- This simple JavaScript/jQuery  uses buttons to modify some elements on the page
+// Requirements: jQuery must be loaded for this script to work.
+//Author:celeste Hernandez
+// Date:2024 May 12
+
+//
+
+// add button to challenge section
+$("#challenge").append("<button id='button-challenge'>Make Special</button>");
+// add a click listener to the challenge button
+$("button-challenge").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#challenge").toggleClass("special");
+});
