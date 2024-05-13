6472d88789f38bd4dc2928523aa0627ed72f3d90
ElfCutieCelly <elfcelly0624@gmail.com>
2024-05-12 21:16:29 -0700

    reah

--- lab9/js/lab9.js
+++ lab9/js/lab9.js
@@ -6,9 +6,24 @@
 // Date:2024 May 12
 
 
+$("#the challenges").append("<button id='button-problems'>Click Me!</button>");
+//add a click listner to the problems button
+$("#button-the challenges").click(function(){
+    //now add (or subtract) the 'special" class to the section
+    $("#the challenges").toggleClass("special");
+})
+
 $("#problems").append("<button id='button-problems'>Click Me!</button>");
 //add a click listner to the problems button
 $("#button-problems").click(function(){
     //now add (or subtract) the 'special" class to the section
     $("#problems").toggleClass("special2");
+})
+
+
+$("#results").append("<button id='button-problems'>Click Me!</button>");
+//add a click listner to the problems button
+$("#button-results").click(function(){
+    //now add (or subtract) the 'special" class to the section
+    $("#results").toggleClass("special3");
 })
