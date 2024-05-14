d617848b3effd012b6e5955e151cd101c65e64a3
ElfCutieCelly <elfcelly0624@gmail.com>
2024-05-14 16:30:27 -0700

    Update livecoding.js

--- experiments2/js/livecoding.js
+++ experiments2/js/livecoding.js
@@ -4,8 +4,8 @@ function getNameAndDisplay(){
     // When the button is pushed, prompt the user to input their name with 
     let name = prompt("Name please:");
     //Ise this input to change the text of the h1 using
-    $('#title').html('Hello' + name)
+    $('#title').html('Hello' + name);
 }
 
 // Attach a click handler to the button within the JS file
-$("#my-button").click(getNameAndDisplay)
+$("#my-button").click(getNameAndDisplay);
