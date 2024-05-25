fce6f0158802d5c4602f6e67e796ad19bc6877ae
ElfCutieCelly <elfcelly0624@gmail.com>
2024-05-25 14:21:29 -0700

    Update lab13.js

--- lab13/js/lab13.js
+++ lab13/js/lab13.js
@@ -6,9 +6,24 @@
 //Author:Celeste Hernandez
 //Created:May 25, 204
 
-maxFactors = 3;
+let str ="";
+if (i % 3 == 0){
+    str += "Fizz";
+}
+if (i % 5 == 0){
+    str += "Buzz";
+}
+if (i % 7 == 0){
+    str += "Boom";
+}
 
-outputEl = document.getElementById("output");
+if (str == ""){
+    console.log(i);
+    $("#output").append("<p class ='num'>" + i);
+} else{
+    console.log(str + '!');
+    $("#output").append("<p class= 'text'>" + str + '!');
+}
 
 function fizzBuzzBoom(maxNums,factorObj){
     //iterate over all of out numbers
