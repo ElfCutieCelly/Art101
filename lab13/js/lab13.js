127f572ac66a5a8daf4df8cc1100ca3d1d7e8573
ElfCutieCelly <elfcelly0624@gmail.com>
2024-05-25 14:34:50 -0700

    Update lab13.js

--- lab13/js/lab13.js
+++ lab13/js/lab13.js
@@ -5,25 +5,29 @@
 
 //Author:Celeste Hernandez
 //Created:May 25, 204
+for (let i = 1; i <=200; i++){
+    let str ="";
+    if (i % 3 == 0){
+        str += "Fizz";
+    }
+    if (i % 5 == 0){
+        str += "Buzz";
+    }
+    if (i % 7 == 0){
+        str += "Boom";
+    }
+    
+    if (str == ""){
+        console.log(i);
+        $("#output").append("<p class ='num'>" + i);
+    } else{
+        console.log(str + '!');
+        $("#output").append("<p class= 'text'>" + str + '!');
+    }
+    
+}
 
-let str ="";
-if (i % 3 == 0){
-    str += "Fizz";
-}
-if (i % 5 == 0){
-    str += "Buzz";
-}
-if (i % 7 == 0){
-    str += "Boom";
-}
 
-if (str == ""){
-    console.log(i);
-    $("#output").append("<p class ='num'>" + i);
-} else{
-    console.log(str + '!');
-    $("#output").append("<p class= 'text'>" + str + '!');
-}
 
 function fizzBuzzBoom(maxNums,factorObj){
     //iterate over all of out numbers
