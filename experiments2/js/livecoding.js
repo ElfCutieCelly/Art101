ca0e180a726301e87c52f863eda2c3be28f49340
ElfCutieCelly <elfcelly0624@gmail.com>
2024-05-28 20:10:08 -0700

    pls

--- experiments2/js/livecoding.js
+++ experiments2/js/livecoding.js
@@ -1,11 +1,11 @@
-console.log('Hello from index.js');
+// console.log('Hello from index.js');
 
-function getNameAndDisplay(){
-    // When the button is pushed, prompt the user to input their name with 
-    let name = prompt("Name please:");
-    //Ise this input to change the text of the h1 using
-    $('#title').html('Hello, ' + name);
-}
+// function getNameAndDisplay(){
+//     // When the button is pushed, prompt the user to input their name with 
+//     let name = prompt("Name please:");
+//     //Ise this input to change the text of the h1 using
+//     $('#title').html('Hello, ' + name);
+// }
 
-// Attach a click handler to the button within the JS file
-$("#my-button").click(getNameAndDisplay);
+// // Attach a click handler to the button within the JS file
+// $("#my-button").click(getNameAndDisplay);
